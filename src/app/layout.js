8204a36e3203329cf492a90561cd6aff66af8540
Tanzim Hossain <tanzimhossain2@gmail.com>
2024-03-26 12:26:46 +0600

    Convert Markdown files to JavaScript, transform documents to JSON using Remark

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { getDocuments } from "@/lib/doc";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +11,8 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+ const allDocuments= getDocuments();
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
